8512e266961c2ebac7b8bd69e8679e30c0a7b62e
mateus.santana <mateus.santana@e-unicred.com.br>
2024-06-07 07:52:39 -0300

    First Commit

---
+++ src/test/java/com/catusoft/demo/CronProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catusoft.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CronProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
